bf0084c874b51ed232d6bad9c96601eac7606005
ANUSREE S S <sreeanu110405@gmail.com>
2023-12-28 21:57:25 +0530

    Tenth Commit

--- react-code/src/App.js
+++ react-code/src/App.js
@@ -1,24 +1,40 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import AxiosGet from './Day 8/AxiosGet';
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-      
-      {/* <ul>
-        <li><Link to='/Home'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-      </ul> */}
-        {/* <Routes>
-          <Route path='/Home' element={<Home/>}></Route>
-          <Route path='/About' element={<About/>}></Route>
-        </Routes>
-         */}
-         <AxiosGet/>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
+import { useState, useMemo } from "react";
+import ReactDOM from "react-dom/client";
+const App = () => {
+const [count, setCount] = useState(0);
+const [todos, setTodos] = useState([]);
+const calculation = useMemo(() => expensiveCalculation(count), [count]);
+const increment = () => {
+setCount((c) => c + 1);
+};
+const addTodo = () => {
+setTodos((t) => [...t, "New Todo"]);
+};
+return (
+<div>
+<div>
+<h2>My Todos</h2>
+{todos.map((todo, index) => {
+return <p key={index}>{todo}</p>;
+})}
+<button onClick={addTodo}>Add Todo</button>
+</div>
+<hr />
+<div>
+Count: {count}
+<button onClick={increment}>+</button>
+<h2>Expensive Calculation</h2>
+{calculation}
+</div>
+</div>
+);
+};
+const expensiveCalculation = (num) => {
+console.log("Calculating...");
+for (let i = 0; i < 1000000000; i++) {
+  num += 1;
+  }
+  return num;
+  };
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
